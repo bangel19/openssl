c8561de71f8c2e4799871b76bc48c112578e09d1
bangel19 <123394216+bangel19@users.noreply.github.com>
2023-06-16 09:31:14 -0400

    Update extensions_clnt.c

--- ssl/statem/extensions_clnt.c
+++ ssl/statem/extensions_clnt.c
@@ -612,7 +612,7 @@ static int add_key_share(SSL *s, WPACKET *pkt, unsigned int curve_id)
     EVP_PKEY *key_share_key = NULL;
     size_t encodedlen = 0;
     uint16_t classical_encodedlen = 0;
-    uint24_t oqs_encodedlen = 0;
+    uint32_t oqs_encodedlen = 0;
     int do_pqc = IS_OQS_KEM_CURVEID(curve_id); /* 1 if post-quantum alg, 0 otherwise */
     int do_hybrid = IS_OQS_KEM_HYBRID_CURVEID(curve_id); /* 1 if post-quantum hybrid alg, 0 otherwise */
     if (s->s3->tmp.pkey != NULL) {
