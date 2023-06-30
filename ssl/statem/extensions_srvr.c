ab935081cc6aed53f0966224aaf0f0721044f9db
bangel19 <123394216+bangel19@users.noreply.github.com>
2023-06-30 10:25:16 -0400

    Update extensions_srvr.c

--- ssl/statem/extensions_srvr.c
+++ ssl/statem/extensions_srvr.c
@@ -1064,8 +1064,8 @@ int tls_parse_ctos_ems(SSL *s, PACKET *pkt, unsigned int context, X509 *x,
    /* if (PACKET_remaining(pkt) != 0) {
         SSLfatal(s, SSL_AD_DECODE_ERROR,
                  SSL_F_TLS_PARSE_CTOS_EMS, SSL_R_BAD_EXTENSION);
-        return 0; */
-    }
+        return 0; 
+    } */
 
     s->s3->flags |= TLS1_FLAGS_RECEIVED_EXTMS;
 
