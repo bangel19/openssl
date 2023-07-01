542c6f30c89e21561090e306b59199d645f563ec
bangel19 <123394216+bangel19@users.noreply.github.com>
2023-07-01 14:32:11 -0400

    Update extensions_srvr.c

--- ssl/statem/extensions_srvr.c
+++ ssl/statem/extensions_srvr.c
@@ -304,17 +304,18 @@ int tls_parse_ctos_sig_algs_cert(SSL *s, PACKET *pkt, unsigned int context,
 
     if (!PACKET_as_length_prefixed_2(pkt, &supported_sig_algs)
             || PACKET_remaining(&supported_sig_algs) == 0) {
-        SSLfatal(s, SSL_AD_DECODE_ERROR,
+       /* SSLfatal(s, SSL_AD_DECODE_ERROR,
                  SSL_F_TLS_PARSE_CTOS_SIG_ALGS_CERT, SSL_R_BAD_EXTENSION);
         return 0;
-    }
+    } */
 
     if (!s->hit && !tls1_save_sigalgs(s, &supported_sig_algs, 1)) {
         SSLfatal(s, SSL_AD_DECODE_ERROR,
                  SSL_F_TLS_PARSE_CTOS_SIG_ALGS_CERT, SSL_R_BAD_EXTENSION);
         return 0;
     }
-
+    }
+   
     return 1;
 }
 
